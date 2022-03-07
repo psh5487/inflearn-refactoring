f9b65440b22b2adffb190d457064ea04d18a8ece
sohyun <psh5487@gmail.com>
2022-03-08 02:25:43 +0900

    refactor: 12_repeated_switches

---
+++ src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/ex/SwitchImprovements.java
@@ -0,0 +1,15 @@
+package me.whiteship.refactoring._12_repeated_swtiches.ex;
+
+public class SwitchImprovements {
+
+    public int vacationHours(String type) {
+        int result;
+        switch (type) {
+            case "full-time": result = 120; break;
+            case "part-time": result = 80; break;
+            case "temporal": result = 32; break;
+            default: result = 0; break;
+        }
+        return result;
+    }
+}
